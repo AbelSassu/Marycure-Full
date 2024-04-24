e35e96a325b7f7e46ecf912acee4a9adbefe4571
Abel Sassu <relinquished@hotmail.it>
2024-04-25 01:33:05 +0200

    add css to reflect Home components changes

--- marycuref/src/components/Hero.jsx
+++ marycuref/src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
             {" "}
             {/* Assicurati che il genitore abbia una posizione relativa o assoluta */}
             <section>
-                <div className="relative h-screen bg-bluscuro z-12 h-[600px] md:h-[720px] lg:h-[770px] xl:h-[850px]">
+                <div className="relative bg-bluscuro z-12 h-[500px] md:h-[730px] lg:h-[780px] xl:h-[860px]">
                     <video
                         autoPlay
                         muted
@@ -29,14 +29,14 @@ function Hero() {
                     </video>
                     {/* Div per scurire, ora copre perfettamente l'area dei video */}
                     <div className="absolute top-0 left-0 w-full h-full bg-bluscuro bg-opacity-45"></div>
-                    <div className="absolute top-28 xs:top-32 sm:top-36 md:top-40 xl:top-60 left-1/2 transform -translate-x-1/2 z-10 w-3/4 md:w-2/3">
-                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-oro mb-4">
+                    <div className="absolute top-24 xs:top-28 sm:top-32 md:top-40 xl:top-60 left-1/2 transform -translate-x-1/2 z-10 w-3/4 md:w-2/3">
+                        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-oro mb-3 sm:mb-5">
                             Prenditi un momento di pausa.
                         </h1>
-                        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-oro mb-4">
+                        <h2 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-oro mb-3 sm:mb-5">
                             Prenditi cura di te stessa.
                         </h2>
-                        <p className="text-xl md:text-3xl lg:text-3xl text-oro font-bold mb-8 lg:pe-32">
+                        <p className="text-lg md:text-3xl lg:text-3xl text-oro font-bold mb-3 sm:mb-8 lg:pe-32">
                             Prenota subito tra la vasta gamma di servizi e
                             scegli quello che più si adatta alle tue esigenze
                         </p>
@@ -47,7 +47,7 @@ function Hero() {
                             href="/Servizi"
                             className="text-white bg-verde hover:text-oro font-medium py-2 px-6 rounded-full transition duration-300 ease-in-out"
                         >
-                            Dai un&apos;occhiata
+                            Sfoglia servizi
                             <FontAwesomeIcon
                                 icon={faChevronRight}
                                 className="ml-2"
